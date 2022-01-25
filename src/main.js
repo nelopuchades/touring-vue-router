66c74dcb9970a147c638e789bcf298f0ae0a3e4a
Nelo Puchades <correo@nelo.dev>
2022-01-25 23:41:06 +0100

    playing with reactive global store

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  // .provide('GStore', GStore)
   .mount('#app')
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,8 +6,15 @@
 <script>
 export default {
   props: ['event'],
+  // inject: ['GStore'],
   methods: {
     register() {
+      // this.GStore.flashMessage = `You are successfully registered for ${this.event.title}`
+
+      // setTimeout(() => {
+      //   this.GStore.flashMessage = ''
+      // }, 3000)
+
       this.$router.push({
         name: 'EventDetails'
       })
